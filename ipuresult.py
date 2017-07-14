f0409c601bfdb478690b07098a5454cf7bf00cd1
Shivank Awasthi <shivankawasthi.cse.msit@gmail.com>
2017-07-15 00:10:33 +1000

    Add files via upload
    This file contains the script that prints result of each student

---
+++ ipuresult.py
@@ -0,0 +1,34 @@
+import requests
+# Python Script to get case details of
+# Supreme Court for a given set of parameters.
+# @uthor: Shivank Awasthi
+# TimeStamp: 13th February, 2017 17:33
+from bs4 import BeautifulSoup
+
+# necessary package imports
+
+srollno = raw_input("Enter starting roll number:")
+erollno = raw_input("Enter ending roll number:")
+
+rollno = int(srollno)
+erollno = int(erollno)
+while(rollno<=erollno):
+    rollno = str(rollno)
+    rollno = "0"+rollno #add two zeros for single digit enroll no
+    res = requests.post('http://ipuresult.com/index.php',data = {'Roll_No':rollno})
+    if(res.status_code==200):
+        if(len(res.content)!=8477):
+           soupBody = BeautifulSoup(res.text,'html.parser')
+           text = soupBody.get_text()
+           pos = text.index("Percentage")
+           print(rollno+"          "+text[pos+12]+text[pos+13]+text[pos+14]+text[pos+15]+text[pos+16]+text[pos+17])
+        else:
+           print(rollno+"          -")
+    rollno = int(rollno)
+    rollno = rollno+200000000
+print("List is complete")
+#print(soupBody.prettify())
+# additional statements for testing of function
+
+#test = get_case_status(selType,caseNo,selYear)
+#print(test)
